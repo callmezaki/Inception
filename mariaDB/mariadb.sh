2e2f905b8cb2ff3caf650ea113a2b9e112c80495
Zakariae Ait sliman <zait-sli@student.1337.ma>
2023-05-02 18:12:30 +0100

    updating mariadb and adding wp

--- mariaDB/mariadb.sh
+++ mariaDB/mariadb.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
 service mysql start
 
+sed -i "s/127.0.0.1\0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql -e "create database zack;"
-mysql -e "create user 'zait-sli'@'%';"
-mysql -e "grant all privileges on zack.* to 'zait-sli'@'%';"
-mysql -e "alter user 'root'@'localhost' identified by '0000';"
-mysql -e "flush privileges;"
+if [ ! -d "/var/lib/mysql/zack" ]; then
+
+    mysql -e "create database zack;"
+    mysql -e "create user 'zait-sli'@'%';"
+    mysql -e "grant all privileges on zack.* to 'zait-sli'@'%';"
+    mysql -e "alter user 'root'@'localhost' identified by '0000';"
+    mysql -e "flush privileges;"
+
+fi
+
 
 
 mysqld
