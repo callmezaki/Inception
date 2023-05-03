8c9006cf4e5a74fbb6e3ed6d1f48e311fbc09d1b
Zakariae Ait sliman <zait-sli@student.1337.ma>
2023-05-03 22:57:18 +0100

    not working

--- mariaDB/mariadb.sh
+++ mariaDB/mariadb.sh
@@ -2,18 +2,18 @@
 
 service mysql start
 
-sed -i "s/127.0.0.1\0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 if [ ! -d "/var/lib/mysql/zack" ]; then
 
-    mysql -e "create database zack;"
-    mysql -e "create user 'zait-sli'@'%';"
-    mysql -e "grant all privileges on zack.* to 'zait-sli'@'%';"
-    mysql -e "alter user 'root'@'localhost' identified by '0000';"
-    mysql -e "flush privileges;"
+    mysql -u root -e "create database zack;"
+    mysql -u root -e "create user 'zait-sli'@'%' identified by '12345';"
+    mysql -u root -e "grant all privileges on zack.* to 'zait-sli'@'%';"
+    mysql -u root -e "alter user 'root'@'localhost' identified by '0000';"
 
 fi
 
+kill $(pidof mysqld)
 
 
 mysqld
---
+++ wordpress/wp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+chmod +x wp-cli.phar 
+mv wp-cli.phar /usr/local/bin/wp
+
+mkdir -p /var/www/html
+cd /var/www/html
+
+wp core download --allow-root --insecure 
+wp config create --dbname=zack --dbuser=zait-sli --dbpass=12345 --dbhost=mariadb --allow-root
+wp core install --url=127.0.0.1 --title=Inception --admin_user=superzack --admin_email=zait-sli@student.1337.ma --skip-email --allow-root
+wp user create zack zait-sli@student.1337.ma --role=author --user-pass=01234 --skip-email --allow-root
