c093a762f8abca4ded0f14ca9497332d510c4613
Zakariae Ait sliman <zait-sli@student.1337.ma>
2023-05-07 20:31:32 +0100

    update

--- mariaDB/mariadb.sh
+++ mariaDB/mariadb.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 service mysql start
-
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-if [ ! -d "/var/lib/mysql/zack" ]; then
+sleep 1
+# if [ ! -d "/var/lib/mysql/zack" ]; then
 
     mysql -u root -e "create database zack;"
-    mysql -u root -e "create user 'zait-sli'@'%' identified by '12345';"
-    mysql -u root -e "grant all privileges on zack.* to 'zait-sli'@'%';"
+    mysql -u root -e "create user 'zait-sli' identified by '12345';"
+    mysql -u root -e "grant all privileges on zack.* to 'zait-sli';"
     mysql -u root -e "alter user 'root'@'localhost' identified by '0000';"
 
-fi
-
-kill $(pidof mysqld)
+# fi
+# mkdir -p /var/run/mysqld/mysqld.sock;
+sleep 1
+# kill $(pidof mysqld)
+service mysql stop
+sleep 1
 
 
 mysqld
