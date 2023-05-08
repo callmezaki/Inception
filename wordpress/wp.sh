61ef6603970f3056feb81d25078b3484606429c3
Zakariae Ait sliman <zait-sli@student.1337.ma>
2023-05-08 19:55:00 +0100

    fixed major errors, now working but needs attention

--- wordpress/wp.sh
+++ wordpress/wp.sh
@@ -4,11 +4,13 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 chmod +x wp-cli.phar 
 mv wp-cli.phar /usr/local/bin/wp
-
+mkdir -p /run/php
 mkdir -p /var/www/html
 cd /var/www/html
 
 wp core download --allow-root --insecure 
 wp config create --dbname=zack --dbuser=zait-sli --dbpass=12345 --dbhost=mariadb --allow-root
 wp core install --url=127.0.0.1 --title=Inception --admin_user=superzack --admin_email=zait-sli@student.1337.ma --skip-email --allow-root
-wp user create zack zait-sli@student.1337.ma --role=author --user-pass=01234 --skip-email --allow-root
+wp user create zack zakariae.1234@gmail.com --role=author --user_pass=01234 --allow-root
+
+exec "$@"
